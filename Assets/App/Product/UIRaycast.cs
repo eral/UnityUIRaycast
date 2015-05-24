ec8bd12fbe0003d44e505ae2d1246e1cd8336c43
eral.r.l <eral.r.l+git@gmail.com>
2015-05-25 03:55:09 +0900

    :heavy_plus_sign:UIRaycast:Support raycast on constant pixel view
    物理解像度ビューに於けるレイレキャスト対応

---
+++ Assets/App/Product/UIRaycast.cs
@@ -0,0 +1,26 @@
+﻿// (C) 2015 ERAL
+// Distributed under the Boost Software License, Version 1.0.
+// (See copy at http://www.boost.org/LICENSE_1_0.txt)
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class UIRaycast {
+
+	public static List<GameObject> RaycastOnConstantPixelView(Vector2 point) {
+		var eventData = new PointerEventData(EventSystem.current);
+		eventData.Reset();
+		eventData.position = point;
+
+		var raycastResults = new List<RaycastResult>();
+		EventSystem.current.RaycastAll(eventData, raycastResults);
+		var result = raycastResults.Where(x=>x.gameObject != null)
+									.Select(x=>x.gameObject)
+									.ToList();
+		return result;
+	}
+
+
+}
---
+++ Assets/App/Test/ConstantPixelTest.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ConstantPixelTest : MonoBehaviour {
+
+	public enum Condition {
+		Hit,
+		NoHit,
+		ThrowSystemArgumentNullException,
+		ThrowUnknownColliderException,
+		ThrowSystemNotImplementedException,
+	}
+	
+	public Condition m_Pass = Condition.Hit;
+	public Vector2 m_TargetPoint = new Vector2(0.5f, 0.5f);
+	public GameObject m_TargetObject;
+
+	void Update() {
+		try {
+			var point = new Vector2(m_TargetPoint.x * Screen.width, m_TargetPoint.y * Screen.height);
+			var raycast_hit = UIRaycast.RaycastOnConstantPixelView(point).FirstOrDefault();
+			if (m_TargetObject != null) {
+				if (m_TargetObject == raycast_hit) {
+					Pass(Condition.Hit);
+				} else {
+					Pass(Condition.NoHit);
+				}
+			} else {
+				if (raycast_hit != null) {
+					Pass(Condition.Hit);
+				} else {
+					Pass(Condition.NoHit);
+				}
+			}
+		} catch (System.ArgumentException) {
+			Pass(Condition.ThrowSystemArgumentNullException);
+		} catch (System.NotImplementedException) {
+			Pass(Condition.ThrowSystemNotImplementedException);
+		} catch {
+			IntegrationTest.Fail(gameObject);
+		}
+	}
+
+	private void Pass(Condition cond) {
+		if (cond == m_Pass) {
+			IntegrationTest.Pass(gameObject);
+		} else {
+			IntegrationTest.Fail(gameObject);
+		}
+	}
+}
